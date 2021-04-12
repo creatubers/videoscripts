05dfc857ea550de2dd9eee181cff49fb0e776334
creatubers <82148467+creatubers@users.noreply.github.com>
2021-04-12 16:18:55 +0200

    Crea la copia de un proyecto en Rclone

--- backuproyecto.ps1
+++ backuproyecto.ps1
@@ -1,7 +1,7 @@
 $NombreDirectorio = Split-Path -Path $pwd -Leaf
 $Year = get-date -Format yyyy
 $DirectorioLocal = $PSScriptRoot
-$DirectorioRemoto = 'creatubers:BACKUP/' + $Year + '/' + $NombreDirectorio
+$DirectorioRemoto = 'remoto:BACKUP/' + $Year + '/' + $NombreDirectorio
 rclone sync $DirectorioLocal $DirectorioRemoto -v
 rclone delete $DirectorioRemoto/backuproyecto.ps1
 # shutdown /s
